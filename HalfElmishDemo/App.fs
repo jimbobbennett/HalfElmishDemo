24b89faf41b69aa2dac341dfa6f87961f7390c4f
Jim Bennett <jim.bennett@mac.com>
2018-06-25 14:36:17 +0100

    First version

---
+++ HalfElmishDemo/App.fs
@@ -0,0 +1,15 @@
+﻿namespace HalfElmishDemo
+
+open Elmish.XamarinForms
+open Xamarin.Forms
+
+type App() =
+    inherit Application()
+
+    do
+        let page = Program.mkSimple MainPage.init MainPage.update MainPage.view
+                    |> Program.withConsoleTrace
+                    |> Program.withStaticView
+                    |> Program.run
+
+        base.MainPage <- page.InitialMainPage
---
+++ HalfElmishDemo/MainPage.xaml.fs
@@ -0,0 +1,33 @@
+﻿namespace HalfElmishDemo
+
+open Elmish.XamarinForms.StaticViews
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type MainPage() =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<MainPage>)
+
+module MainPage =
+    type Model =
+        {
+            Count : int
+        }
+
+    type Msg =
+        | Increment
+
+    let init() =
+        { Count = 0 }
+
+    let update msg model =
+        match msg with
+        | Increment -> {model with Count = model.Count+1 }
+
+    let view () =
+        MainPage(),
+        [
+            "Message" |> Binding.oneWay (fun m -> sprintf "Clicked = %d" m.Count)
+            "Increment" |> Binding.msg Increment
+        ]
+
